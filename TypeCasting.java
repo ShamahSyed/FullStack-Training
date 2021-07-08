e31b999a6129ab9190ea0138e22aff967efe299b
ShamahSyed <86568208+ShamahSyed@users.noreply.github.com>
2021-07-08 07:01:09 +0400

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,22 @@
+package knightingale;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		byte = 10;
+		
+		int i=b;
+		
+		byte c=i; 
+		
+		byte d = (byte)i;
+		
+		byte x=10;
+		byte y=20;
+		
+		byte sum=x*y;
+	
+	}
+	
+}
+
+
